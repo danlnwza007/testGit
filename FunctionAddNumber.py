52a1b57bd4e4db667f35270cd7c359464c1f8dd3
danlnwza007 <68460767+danlnwza007@users.noreply.github.com>
2020-08-08 16:44:54 +0700

    Create FunctionAddNumber.py

---
+++ FunctionAddNumber.py
@@ -0,0 +1,12 @@
+def plusnumber(x,y):
+    print(x+y)
+def minusnumber(x,y):
+    print(x-y)
+def multiplynumber(x,y):
+    print(x*y)
+def dividenumber(x,y):
+    print(x%y)
+plusnumber(100,25)
+minusnumber(100,25)
+multiplynumber(100,25)
+dividenumber(100,25)
